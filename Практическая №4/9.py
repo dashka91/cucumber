a6e6107f71185b548e032ff56759be8f36d7f8a9
dashka91 <dasacuguevskaa89962@gmail.com>
2024-10-19 22:02:46 +0300

    Add files via upload

--- Практическая №4/9.py
+++ Практическая №4/9.py
@@ -1,13 +1,9 @@
-N=int(input())
+n=int(input())
 def f(n):
-    a,b=0,1
     summa=0
-    for i in range(n):
-        summa+=a
-        a,b=b,a+b
+    r=1
+    for i in range(1,n+1):
+        r *=i
+        summa += r
     return summa
-if N>0:
-    r=f(N)
-    print(f'сумма {N} чисел Фибоначчи равна: {r}')
-else:
-    print('введите число больше 0')
+print(f'сумма факториалов равна {f(n)}')

6fd7b8f21537dd86ac4b37ba807a4405f2d81fdd
dashka91 <dasacuguevskaa89962@gmail.com>
2024-11-11 20:03:48 +0300

    Add files via upload

---
+++ 8.py
@@ -0,0 +1,9 @@
+n=100
+def f(n):
+    r=[]
+    for m in range(1,n+1):
+        d=[int(b) for b in str(m)]
+        if all(b!=0 and m%b==0 for b in d):
+            r.append(m)
+    return r
+print(f(n))

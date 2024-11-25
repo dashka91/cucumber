7e42f4aad5ea40ad9055c1ff0496dc9743aa0ecc
dashka91 <dasacuguevskaa89962@gmail.com>
2024-11-25 16:23:35 +0300

    Create 9.2.py

---
+++ Практическая №9/9.2.py
@@ -0,0 +1,11 @@
+def f(m):
+  n=len(m)
+  t=[[0]*n for v in range(n)]
+  for i in range(n):
+    for j in range(n):
+      t[j][i]=m[i][j]
+      return t
+m=[[1,2,3],[4,5,6],[7,8,9]]
+f=f(m)
+for r in f:
+  print(r)
